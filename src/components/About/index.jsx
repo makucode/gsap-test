67a42757df1957d9e088e01fbe517e26cde5310a
maku <makucode@gmail.com>
2022-03-14 23:28:21 +0100

    added scroll effects

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,11 +1,66 @@
-import React from "react";
+import React, { useLayoutEffect, useRef } from "react";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
 import ChevronRight from "../Icons/ChevronRight";
 import aboutImg from "../../assets/imgs/about.bmp";
 import "./style.scss";
 
 const About = () => {
+    const aboutRef = useRef();
+    const q = gsap.utils.selector(aboutRef);
+    const tl = useRef();
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    useLayoutEffect(() => {
+        gsap.to(q(".AboutCaption h2"), {
+            scrollTrigger: {
+                trigger: q(".AboutCaption h2"),
+                start: "top bottom",
+                end: "top center",
+                scrub: 0.5,
+            },
+            x: 0,
+            opacity: 1,
+            filter: "blur(0px)",
+        });
+        gsap.to(q(".AboutCaption button"), {
+            scrollTrigger: {
+                trigger: q(".AboutCaption button"),
+                start: "top bottom",
+                end: "top center",
+                scrub: 0.5,
+            },
+            y: 0,
+            opacity: 1,
+            filter: "blur(0px)",
+        });
+        gsap.to(q(".AboutCaption p"), {
+            scrollTrigger: {
+                trigger: q(".AboutCaption p"),
+                start: "top bottom",
+                end: "top center",
+                scrub: 1,
+            },
+            y: 0,
+            opacity: 1,
+            scale: 1,
+        });
+        gsap.to(q(".AboutImage img"), {
+            scrollTrigger: {
+                trigger: q(".AboutImage"),
+                start: "top bottom",
+                end: "center center",
+                scrub: 0.5,
+            },
+            opacity: 1,
+            filter: "blur(0px)",
+            x: 0,
+        });
+    }, []);
+
     return (
-        <section className="About">
+        <section className="About" ref={aboutRef}>
             <div className="AboutContent">
                 <div className="AboutCaption">
                     <h2> At vero eos et accusam.</h2>
